6aa9b86e1bf518f49748249572db405b5846b264
Kuba Bolek <jakubbolek02@gmail.com>
2025-01-05 15:50:40 +0100

    Added Loading page.

--- src/pages/Backend.tsx
+++ src/pages/Backend.tsx
@@ -2,47 +2,53 @@ import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import logo from '../assets/logo.svg';
 import styles from '../styles/styles';
+import Loading from '../components/Loading'; 
 
 const Backend: React.FC = () => {
   const [message, setMessage] = useState<string>('');
   const [loading, setLoading] = useState<boolean>(true);
   const [error, setError] = useState<string | null>(null);
+  const [showReactLogo, setShowReactLogo] = useState<boolean>(false);
 
   useEffect(() => {
-    // Check backend connection
-    const fetchBackendMessage = async () => {
-      try {
-        const response = await axios.get('https://localhost:44336/api/messages');
-        setMessage(response.data.message || 'Backend connection successful!');
-      } catch (err) {
-        setError('Failed to connect to the backend.');
-      } finally {
-        setLoading(false);
-      }
-    };
+    const timer = setTimeout(() => {
+      const fetchBackendMessage = async () => {
+        try {
+          const response = await axios.get('https://localhost:44336/api/messages');
+          setMessage(response.data.message || 'Backend is connected properly');
+        } catch (err) {
+          setError('Failed to connect to the backend.');
+        } finally {
+          setLoading(false);
+          setShowReactLogo(true);
+        }
+      };
 
-    fetchBackendMessage();
+      fetchBackendMessage();
+    }, 5000); 
+
+    return () => clearTimeout(timer);
   }, []);
 
   return (
     <div style={styles.app}>
       <header style={styles.appHeader}>
-        <img src={logo} style={styles.appLogo} alt="logo" />
         {loading ? (
-          <p>Checking backend connection...</p>
+          <>
+            <Loading />
+            <p style={{ color: '#fff', fontSize: '1.5em', marginTop: '1em' }}>Loading data...</p>
+          </>
         ) : error ? (
           <p style={{ color: 'red' }}>{error}</p>
         ) : (
-          <p style={{ color: 'green' }}>{message}</p>
+          <>
+            {showReactLogo && (
+              <img src={logo} alt="React Logo" style={{ width: '10em', height: '10em', marginBottom: '1em' }} />
+            )}
+            <p style={{ color: 'green', fontSize: '1.5em' }}>{message}</p>
+
+          </>
         )}
-        <a
-          style={styles.appLink}
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
     </div>
   );
